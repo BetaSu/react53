ff9675728763c09055b2d70a97ee6b8287e84379
kasong <313439271@qq.com>
2021-07-29 18:27:24 +0800

    feat: 文案

--- bin/cli.js
+++ bin/cli.js
@@ -5,21 +5,20 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const ora = require('ora');
 const {exec} = require('child_process');
-const {name} = require(path.resolve(__dirname, '../package.json'));
+const {name, description} = require(path.resolve(__dirname, '../package.json'));
 
 function execDefer(command) {
   return new Promise(function(resolve, reject) {
-    exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout) => {
       if (error) {
         return reject(error);
       }
-
       resolve(stdout.trim());
     });
   });
 }
 
-console.log(chalk.yellow(`欢迎使用${name} —— 交互式React进阶手册`));
+console.log(chalk.yellow(`欢迎使用${name} —— ${description}`));
 inquirer.prompt([
   {
     type: 'input',
@@ -33,23 +32,25 @@ inquirer.prompt([
     type: 'list',
     name: 'mirror',
     message: '是否使用镜像？',
-    choices: [{
-      name: '否',
-      value: undefined
-    }, {
-      name: '淘宝镜像',
-      value: 'http://registry.npm.taobao.org'
-    }]
+    choices: [
+      {
+        name: '淘宝镜像',
+        value: 'http://registry.npm.taobao.org'
+      },{
+        name: '否',
+        value: undefined
+      }
+    ]
   },
 ]).then(async ({dir, mirror}) => {
   const spinner = ora('正在初始化项目');
   spinner.start();
 
   try {
-    await execDefer(`git clone git@github.com:BetaSu/react53.git ${dir}`);
+    await execDefer(`git clone https://github.com/BetaSu/react53.git ${dir}`);
     spinner.text = '正在安装依赖';
     await execDefer(`cd ${dir} && npm install ${mirror ? `--registry=${mirror}` : ''}`);
-    spinner.succeed('初始化项目完成');
+    spinner.succeed('安装妥了，开始愉快的学习吧！');
   } catch(e) {
     spinner.fail('初始化项目失败');
     console.log(chalk.red(e));
